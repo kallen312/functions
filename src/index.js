637fe8f2c163aadf7989a514c9e4f6d82074377b
kallen312 <73615486+kallen312@users.noreply.github.com>
2021-10-20 18:56:55 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+`;
+function favColor(){
+  if (myfavColor === color){
+    return ("nice, that's my favorite color too!")
+  }
+  else {
+    return ("oh cool my favorite color is blue.")
+  }
+}
+
+let myfavColor = "red";
+
+let color = "pink";
+
+console.log(favColor);
+console.log(color);
